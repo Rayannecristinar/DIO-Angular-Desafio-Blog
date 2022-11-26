c84594645fd25b6eca09a2ce0d08a0b50905ef6c
Rayannecristinar <rayanne.cristinar@gmail.com>
2022-11-26 18:54:19 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Naruto",
+        "description": "Naruto Uzumaki é um shinobi de Konohagakure do clã Uzumaki e protagonista homônimo da franquia Naruto. Desde seu nascimento, ele se tornou jinchūriki das Nove-Caudas, um destino que o levou a ser condenado e negligenciado por toda a aldeia durante sua infância. Depois de entrar para o Time 7, ao lado de Sakura Haruno, Sasuke Uchiha e sob comando de Kakashi Hatake, Naruto trabalhou duro para ganhar o respeito e o reconhecimento da aldeia, com o sonho de se tornar Hokage. Nos anos seguintes, Naruto torna-se um ninja capaz, que é eventualmente considerado como um herói pelas pessoas de sua aldeia, e mais tarde, pelo mundo shinobi em geral, renomado como o Herói da Vila Oculta da Folha. Posteriormente, Naruto descobre ser a reencarnação do espírito de Ashura e anos depois da Quarta Guerra Mundial Shinobi, Naruto realiza seu sonho e se torna o Sétimo Hokage. Naruto se casa com Hinata Hyūga e tem dois filhos com ela, Boruto Uzumaki e Himawari Uzumaki.",
+        "photoCover": "http://faizalfi.files.wordpress.com/2010/03/naruto-character-2.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Konohagakure ou Vila Oculta da Folha",
+        "description": "Konohagakure (literalmente significa: Folha Oculta) ou Vila Oculta da Folha é a vila oculta do País do Fogo. Como a aldeia de um dos Cinco Grandes Países Shinobi, Konoha tem um kage como seu líder, conhecido como Hokage, dos quais tem sete em sua história. A aldeia reside no fundo de uma floresta na base de uma montanha conhecida como Monumento Hokage, ao qual tem os rostos gravados de todos aqueles que ocuparam a residência do Hokage. Apesar de sua localização, a vila é parcialmente independente com o Hokage no topo do seu sistema de governo. O Daimyō do País do Fogo se encontra acima do Hokage em questão de poder político, mas a relação entre o país e a vila é mais próxima de uma aliança do que de senhor e vassalo, visto que o poder bélico do País do Fogo depende dos shinobi de Konoha. Embora geralmente vista como a mais poderosa das aldeias shinobi, Konoha tem desfrutado de muitos anos de relativa paz e estabilidade.",
+        "photoCover": "https://nerdhits.com.br/wp-content/uploads/2020/05/konoha.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Sunagakure ou Vila Oculta da Areia",
+        "description": "Sunagakure (literalmente significa: Areia Oculta) ou Vila Oculta da Areia é a vila oculta do País do Vento. Como uma vila de um dos Cinco Grandes Países Shinobi, Sunagakure tem um kage como seu líder conhecido como Kazekage, dos quais existiram cinco em sua história. Ser cercada por desertos oferece para a vila um obstáculo natural contra invasões, já que poucas potências estrangeiras estariam dispostas a suportar as tempestades de areia e a escassez de água comuns à Suna. A própria vila situa-se num vale fortificado atrás de uma falésia de rochas, com passagem para dentro e fora da vila restrita a uma única fissura entre duas falésias, tornando a área muito difícil de ser atacada pelo solo. Apesar disso, nos arredores e bordas da vila existe vegetação verdejante. As construções de dentro da vila aparentam ser constituídas de argila ou estuque, os quais ajudariam a manter um clima ameno dentro dessas construções.",
+        "photoCover": "https://i.imgur.com/GZWRh.png"
+    },
+    {
+        "id":"4",
+        "title": "Kumogakure ou Vila Oculta da Nuvem",
+        "description": "Kumogakure (literalmente significa: Nuvem Oculta) ou Vila Oculta da Nuvem é a vila oculta do País do Relâmpago. Como um dos Cinco Grandes Países Shinobi, Kumogakure tem um Kage como o seu líder, conhecido como Raikage, tendo cinco durante toda sua história. A aldeia está localizada em uma cadeia de montanhas altas, e é, literalmente escondida nas nuvens. O Raikage trabalha em uma grande estrutura azul incorporada na montanha mais alta.",
+        "photoCover":"https://pm1.narvii.com/6385/3427df04c30bce6249e27d3796d81632e8bc46c6_00.jpg"
+    }
+]
